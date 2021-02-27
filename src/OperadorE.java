c76c74eccdecd8de18941bd99fee3475f4587c0b
TheAwesomeCake <luc45.2004@gmail.com>
2021-02-26 21:01:13 -0300

    Implementação do operador E

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -13,7 +13,11 @@ public class OperadorE {
 		senha = leitor.next();	
 		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
 		
-		
+		if (usuario.equalsIgnoreCase("administrador") && senha.equals("123")) {
+			System.out.println("Acesso permitido");
+		}else {
+			System.out.println("Acesso negado");
+		}
 		
 		leitor.close();
 
